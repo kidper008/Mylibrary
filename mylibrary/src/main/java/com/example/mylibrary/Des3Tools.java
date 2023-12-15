8f231f625a2feff85dbe901e83c16789c1ec2cf4
sjt <327495993@qq.com>
2023-12-15 08:57:21 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/Des3Tools.java
@@ -0,0 +1,13 @@
+package com.example.mylibrary;
+
+/**
+ * @author Turbo
+ * @date 2019/5/27.
+ */
+public class Des3Tools {
+
+    public static void systemPrint(String str){
+        System.out.println("pppp===="+str);
+    }
+
+}
